1252037ba3c33c0d2e6e17b1435270c412caba8c
Ashesh <83585384+ashesh790@users.noreply.github.com>
2022-09-10 20:12:53 +0530

    Sub content bar

--- QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/urls.py
+++ QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from office_view import views
 
+from office_view import views
 
 urlpatterns = [
     path('', views.index),
-    path('dashboard', views.dashboard)
+    path('dashboard', views.dashboard), 
+    path('report', views.report),
+    path('qr_code', views.qr_code),
+    path('create-user', views.create_user),
+    path('change-password', views.change_password) 
 ]
--- QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/views.py
+++ QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/views.py
@@ -7,4 +7,19 @@ def index(request):
 
 # dashboard
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'dashboard.html') 
+
+
+def report(request):
+    return render(request, 'report.html')   
+
+
+def qr_code(request):
+    return render(request, 'qr_code.html') 
+
+
+def create_user(request):
+    return render(request, 'create_user.html') 
+
+def change_password(request):
+    return render(request, 'change_pasword.html')
