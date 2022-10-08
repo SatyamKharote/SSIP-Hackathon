f38a18c93125ed6c21d53d1ecd1a26878f4fd849
Satyam <satyamkharote123@gmail.com>
2022-10-08 09:13:16 +0530

    feer

--- QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/settings.py
+++ QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'testerweb1122@gmail.com'
+EMAIL_HOST_PASSWORD = 'Testerweb@2211' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/urls.py
+++ QR_Code_Based_Citizen_Feedback_System_For_Gujarat_Police/office_view/urls.py
@@ -7,6 +7,7 @@ from office_view import views
 urlpatterns = [
     path('', views.index),
     path('app', views.dashboard), 
+    path('add_question', views.add_question),
     path('report', views.report),
     path('qr_code', views.qr_code),
     path('create-user', views.create_user),
